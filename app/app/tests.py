93a04cb0ba1192c2bab6e125dd53a465aec720d0
adedayo okubanjo <dayookubanjo@yahoo.com>
2022-07-21 07:19:41 -0400

    Convert indentation to space

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,10 +8,10 @@ from . import calc
 
 
 class ViewTests(SimpleTestCase):
-	def test_addition(self):
-		res = calc.add(5, 7)
-		self.assertEqual(res, 12)
+    def test_addition(self):
+        res = calc.add(5, 7)
+        self.assertEqual(res, 12)
 
-	def test_subtract(self):
-		res = calc.subtract(12, 4)
-		self.assertEqual(res, 8)
+    def test_subtract(self):
+        res = calc.subtract(12, 4)
+        self.assertEqual(res, 8)
