4cd12fe351c43b73703ec480f0a5d130dc374d04
adedayo okubanjo <dayookubanjo@yahoo.com>
2022-07-21 06:59:50 -0400

    Postgres database packages installed

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class ViewTests(SimpleTestCase):
+	def test_addition(self):
+		res = calc.add(5,7)
+		self.assertEqual(res, 12)
+
+	def test_subtract(self):
+		res = calc.subtract(12,4)
+		self.assertEqual(res, 8)
